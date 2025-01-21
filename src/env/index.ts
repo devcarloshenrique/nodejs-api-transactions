8e13b9044cfa65ace1b245a12d4eca495b88935f
devcarloshenrique <devcarloshenrique@gmail.com>
2025-01-21 18:46:21 -0300

    chore: production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,11 +5,10 @@ const envPath = process.env.NODE_ENV === 'test' ? '.env.test' : undefined
 config({ path: envPath })
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(['development', 'test', 'production'])
-        .default('production'),
+    NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+    DATABASE_CLIENT: z.enum(['sqlite','pg']),
     DATABASE_URL: z.string(),
     PORT: z.coerce.number().default(3333)
-
 })
 
 const _env = envSchema.safeParse(process.env)
